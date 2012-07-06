ae18207decdfa6dd53948415419c4de38a2d5565
Kirill <kirstyts@gmail.com>
2012-07-06 18:52:51 +0200

    fuzzywuzzy is too slow for this task, will try to use sqlite %like% for matching

--- main.py
+++ main.py
@@ -30,8 +30,51 @@ class MovieCollection:
         return len(self.movies)
 
 
+class Torrent:
+    title = ""
+    magnet = ""
+    seeds = 0
+
+    def __init__(self, t, m):
+        self.title = t
+        self.magnet = m
+
+    def get_resolution():
+        return "720p"
+
+
+class TorrentCollection:
+    torrents = []
+    titles = []  # used by fuzzywuzzy process
+
+    def __init__(self):
+        for l in open('b3_verified.txt', 'r'):
+            s = l.split('|')
+            mag = s[0]
+            title = s[1]
+            cat = s[2]
+            if cat != 'Video Movies':  # others are not movies
+                continue
+            self.torrents.append(Torrent(title, mag))
+            self.titles.append(title)
+
+    def get_size(self):
+        return len(self.torrents)
+
+    def findMatchingTorrents(self, movie):
+        from fuzzywuzzy import process
+        print process.extract(movie.title, self.titles, limit=2)
+
+
 def main():
     col = MovieCollection('ratings.list')
     print col.get_size()
+
+    tc = TorrentCollection()
+    print tc.get_size()
+
+    m1 = MovieCollection.movies[0]  # random first movie
+    tc.findMatchingTorrents(m1)
+
 
 main()
