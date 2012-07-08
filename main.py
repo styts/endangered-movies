28128b104febb277869781fe2721438283b2055c
Kirill <kirstyts@gmail.com>
2012-07-08 15:13:02 +0200

    DBManager class

--- main.py
+++ main.py
@@ -22,161 +22,148 @@ thresh_rat = 3.0
 thresh_votes = 5000
 
 
-def _clear_db():
-    conn = sqlite3.connect('sqlite.db')
-    cursor = conn.cursor()
-    cursor.execute('DROP TABLE movies')
-    cursor.execute('DROP TABLE torrents')
-    conn.commit()
-    cursor.execute("""CREATE TABLE movies(title TEXT PRIMARY KEY,
-            rating REAL, votes INTEGER, imdb_id TEXT)""")
-    cursor.execute("""CREATE TABLE torrents(magnet TEXT PRIMARY KEY,
-            title TEXT, seeds INTEGER)""")
-    conn.commit()
-    cursor.close()
+class DBManager:
+    def __init__(self):
+        self.conn = sqlite3.connect('sqlite.db')
+        self.cursor = self.conn.cursor()
 
-def populate_imdb_ids():
-    conn = sqlite3.connect('sqlite.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT title FROM movies WHERE imdb_id IS NULL')
-    rows = cursor.fetchall()
-    for r in rows:
-        t = r[0]
-        print t
-        imdb_id = get_imdb_id(t)
-        cursor.execute('UPDATE movies SET imdb_id = ? WHERE title = ?',
-                (imdb_id, t))
-        conn.commit()
+    def __del__(self):
+        self.cursor.close()
+        self.conn.commit()
 
+    def _clear_db(self):
+        self.cursor.execute('DROP TABLE movies')
+        self.cursor.execute('DROP TABLE torrents')
+        self.conn.commit()
+        self.cursor.execute("""CREATE TABLE movies(title TEXT PRIMARY KEY,
+                rating REAL, votes INTEGER, imdb_id TEXT)""")
+        self.cursor.execute("""CREATE TABLE torrents(magnet TEXT PRIMARY KEY,
+                title TEXT, seeds INTEGER)""")
+        self.conn.commit()
 
-def populate_db():
-    global thresh_rat, thresh_votes
+    def populate_imdb_ids(self):
+        self.cursor.execute('SELECT title FROM movies WHERE imdb_id IS NULL')
+        rows = self.cursor.fetchall()
+        for r in rows:
+            t = r[0]
+            print t
+            imdb_id = get_imdb_id(t)
+            self.cursor.execute('UPDATE movies SET imdb_id = ? WHERE title = ?',
+                    (imdb_id, t))
+            self.conn.commit()
 
-    _clear_db()
+    def populate_db(self, thresh_rat, thresh_votes):
+        self._clear_db()
 
-    conn = sqlite3.connect('sqlite.db')
-    cursor = conn.cursor()
+        ### MOVIES
+        rs = parse_ratings('data/ratings.list')
 
-    ### MOVIES
-    rs = parse_ratings('data/ratings.list')
+        for title in rs:
+            s = rs[title]
+            r = s[0]
+            v = s[1]
+            if title[0] == '"':  # it's a show
+                continue
+            if "(VG)" in title:  # it's a video game
+                continue
+            if r >= thresh_rat and v >= thresh_votes:
+                self.cursor.execute('INSERT INTO movies VALUES(?, ?, ?)',
+                    (title.decode('utf-8'), r, v))
 
-    for title in rs:
-        s = rs[title]
-        r = s[0]
-        v = s[1]
-        if title[0] == '"':  # it's a show
-            continue
-        if "(VG)" in title:  # it's a video game
-            continue
-        if r >= thresh_rat and v >= thresh_votes:
-            cursor.execute('INSERT INTO movies VALUES(?, ?, ?)',
-                (title.decode('utf-8'), r, v))
+        ### TORRENTS
+        for l in open('data/b3_verified.txt', 'r'):
+            s = l.split('|')
+            mag = s[0]
+            title = s[1]
+            cat = s[2]
+            if cat != 'Video Movies':  # others are not movies
+                continue
+            self.cursor.execute('INSERT INTO torrents VALUES(?, ?, 0)',
+                    (mag, title.decode('utf-8')))
 
-    ### TORRENTS
-    for l in open('data/b3_verified.txt', 'r'):
-        s = l.split('|')
-        mag = s[0]
-        title = s[1]
-        cat = s[2]
-        if cat != 'Video Movies':  # others are not movies
-            continue
-        cursor.execute('INSERT INTO torrents VALUES(?, ?, 0)',
-                (mag, title.decode('utf-8')))
+        self.conn.commit()
 
-    conn.commit()
+        ### TORRENTS SEEDS
+        for l in open('data/b3_e003_torrents.txt', 'r'):
+            s = l.split('|')
+            mag = s[0]
+            seeds = s[1]
 
-    ### TORRENTS SEEDS
-    for l in open('data/b3_e003_torrents.txt', 'r'):
-        s = l.split('|')
-        mag = s[0]
-        seeds = s[1]
+            if seeds != '0':
+                self.cursor.execute('UPDATE torrents SET seeds = ? WHERE magnet = ?',
+                        (seeds, mag))
 
-        if seeds != '0':
-            cursor.execute('UPDATE torrents SET seeds = ? WHERE magnet = ?',
-                    (seeds, mag))
+            if seeds == '10':  # for better overview, just a few sporadic commits
+                self.conn.commit()
 
-        if seeds == '10':  # for better overview, just a few sporadic commits
-            conn.commit()
+        self.conn.commit()
+        self.cursor.execute('DELETE FROM torrents WHERE seeds == 0')
 
-    conn.commit()
-    cursor.execute('DELETE FROM torrents WHERE seeds == 0')
+        self.conn.commit()
+        self.cursor.close()
 
-    conn.commit()
-    cursor.close()
+    def do_match(self):
+        def likify(t, y):
+            t = re.sub('\(.*?\)$', '', t).strip()
+            slug = unicodedata.normalize('NFKD', t)
+            slug = slug.encode('ascii', 'ignore').lower()
+            slug = re.sub(r'[^a-z0-9]+', '-', slug).strip('-')
+            slug = re.sub(r'[-]+', '%', slug)
+            m_like = '%%%s%%%s%%' % (slug, y)
+            return m_like
 
-
-def likify(t, y):
-    t = re.sub('\(.*?\)$', '', t).strip()
-    slug = unicodedata.normalize('NFKD', t)
-    slug = slug.encode('ascii', 'ignore').lower()
-    slug = re.sub(r'[^a-z0-9]+', '-', slug).strip('-')
-    slug = re.sub(r'[-]+', '%', slug)
-    m_like = '%%%s%%%s%%' % (slug, y)
-    return m_like
-
-
-def do_match():
-    conn = sqlite3.connect('sqlite.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT title, rating FROM movies ORDER BY rating DESC')
-    movies = cursor.fetchall()
-    for m in movies:
-        t = m[0]
-        r = m[1]
-        try:
-            y = re.search('\((\d+).*?\)', t).groups(0)[0]
-        except:
-            y = ''
-        likified = likify(t, y)
-        #print likified
-        cursor.execute("""SELECT seeds FROM torrents WHERE title LIKE "%s"
-                ORDER BY seeds DESC""" % likified)
-        torrents = cursor.fetchall()
-        try:
-            s = int(torrents[0][0])
-        except:
-            s = 0
-        if s == 0:
-            print r, likified, t, torrents
+        self.cursor.execute('SELECT title, rating FROM movies ORDER BY rating DESC')
+        movies = self.cursor.fetchall()
+        for m in movies:
+            t = m[0]
+            r = m[1]
+            try:
+                y = re.search('\((\d+).*?\)', t).groups(0)[0]
+            except:
+                y = ''
+            likified = likify(t, y)
+            #print likified
+            self.cursor.execute("""SELECT seeds FROM torrents WHERE title LIKE "%s"
+                    ORDER BY seeds DESC""" % likified)
+            torrents = self.cursor.fetchall()
+            try:
+                s = int(torrents[0][0])
+            except:
+                s = 0
+            if s == 0:
+                print r, likified, t, torrents
 
 
-def print_stats():
-    conn = sqlite3.connect('sqlite.db')
-    cursor = conn.cursor()
+    def print_stats(self):
+        self.cursor.execute('SELECT count(*) FROM movies')
+        n_movies = self.cursor.fetchone()[0]
+        print 'Movies:', n_movies
 
-    cursor.execute('SELECT count(*) FROM movies')
-    n_movies = cursor.fetchone()[0]
-    print 'Movies:', n_movies
+        self.cursor.execute('SELECT count(*) FROM torrents')
+        n_torrents = self.cursor.fetchone()[0]
+        print 'Torrents:', n_torrents
 
-    cursor.execute('SELECT count(*) FROM torrents')
-    n_torrents = cursor.fetchone()[0]
-    print 'Torrents:', n_torrents
-
-    cursor.close()
-
-
-def print_list():
-    conn = sqlite3.connect('sqlite.db')
-    cursor = conn.cursor()
-
-    cursor.execute("""SELECT title, rating, votes FROM movies
-            ORDER BY rating DESC""")
-    for m in cursor.fetchall():
-        print m[1], m[2], m[0]
-
+    def print_list(self):
+        self.cursor.execute("""SELECT title, rating, votes FROM movies
+                ORDER BY rating DESC""")
+        for m in self.cursor.fetchall():
+            print m[1], m[2], m[0]
 
 def main():
+    global thresh_rat, thresh_votes
+    dbm = DBManager()
+
     args = docopt.docopt(__doc__, version='0.1.1rc')
     if args['--list']:
-        print_list()
+        dbm.print_list()
     if args['--stats']:
-        print_stats()
+        dbm.print_stats()
     if args['--imdbs']:
-        populate_imdb_ids()
+        dbm.populate_imdb_ids()
     if args['--match']:
-        do_match()
+        dbm.do_match()
     if args['--populate']:
-        populate_db()
+        dbm.populate_db(thresh_rat, thresh_votes)
 
 if __name__ == '__main__':
     main()
